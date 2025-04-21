c5cd772f651c46e7baff222eaca7512565fe7446
Caroline <johns83c@cmich.edu>
2025-04-21 19:22:36 -0400

    created fridge menu and script for opening

--- Assets/Scenes/InitialScene/UI/Fridge/FridgeMenu.cs
+++ Assets/Scenes/InitialScene/UI/Fridge/FridgeMenu.cs
@@ -1,25 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FridgeMenu : MonoBehaviour
 {
-    public GameObject FridgeSprite;
+    public GameObject fridgeBG;
+    public Button xFridgeButton;
+    public GameObject[] fridgeItems;
+
+    private bool isOpen = false;
+
+    void Awake()
+    {
+        fridgeBG.SetActive(false);
+        xFridgeButton.gameObject.SetActive(false);
+        SetItemsActive(false);
+    }
 
     void Start()
     {
-        FridgeSprite.SetActive(false); 
+        // closes fridge by pressing the x button shown in menu
+        xFridgeButton.onClick.AddListener(CloseFridge);
     }
 
     void Update()
-{
-    if (Input.GetKeyDown(KeyCode.E))
     {
-        Debug.Log("E was pressed!");
-        FridgeSprite.SetActive(!FridgeSprite.activeSelf);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleFridge();
+        }
+    }
+
+    void ToggleFridge()
+    {
+        isOpen = !isOpen;
+        fridgeBG.SetActive(isOpen);
+        xFridgeButton.gameObject.SetActive(isOpen);
+        SetItemsActive(isOpen);
+    }
+
+   public void CloseFridge()
+{
+    Debug.Log("CloseFridge() clicked!");
+
+    isOpen = false;
+    fridgeBG.SetActive(false);
+    xFridgeButton.gameObject.SetActive(false);
+    SetItemsActive(false);
+}
+
+
+    void SetItemsActive(bool active)
+    {
+        foreach (GameObject item in fridgeItems)
+        {
+            item.SetActive(active);
+        }
     }
 }
-
-}
-
-
