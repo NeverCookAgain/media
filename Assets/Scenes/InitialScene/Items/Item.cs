ec1098b8ab65e386b876dad0a778f6a067d4b384
Christian Toney <toney1c@cmich.edu>
2025-04-21 22:26:52 -0400

    Develop feature: Cooking • Burn item over time (#159)

--- Assets/Scenes/InitialScene/Items/Item.cs
+++ Assets/Scenes/InitialScene/Items/Item.cs
@@ -1,12 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
+
+public enum BurnLevel {
+  NotCooked,
+  Cooked,
+  Burnt
+}
 
 public abstract class Item : MonoBehaviour
 {
 
     public Sprite Sprite;
+    protected BurnLevel burnLevel = BurnLevel.NotCooked;
+    public Dictionary<BurnLevel, Color32> colors = new();
+    public UnityEvent onBurnLevelChange;
+
+    void Awake() {
+
+      colors.Add(BurnLevel.NotCooked, new Color32(255, 255, 255, 255));
+      colors.Add(BurnLevel.Cooked, new Color32(252, 224, 146, 255));
+      colors.Add(BurnLevel.Burnt, new Color32(51, 43, 19, 255));
+
+    }
+
+    public void SetBurnLevel(BurnLevel burnLevel) {
+
+      this.burnLevel = burnLevel;
+      this.onBurnLevelChange.Invoke();
+
+    }
+
+    public BurnLevel GetBurnLevel() {
+
+      return burnLevel;
+
+    } 
 
     public abstract void Activate();
 }
